a12eb81b975b928942f0a4daac7d6fc22d9c8deb
Umang Sharma <umng01@gmail.com>
2021-09-21 21:48:26 +0530

    JS Math⭐️

---
+++ SheCodes Plus/Challenge14/src/index.js
@@ -0,0 +1,13 @@
+let price = 4.5;
+price = Math.round(price);
+console.log(price);
+
+price = Math.floor(4.5);
+console.log(price);
+
+price = Math.ceil(4.5);
+console.log(price);
+
+console.log(Math.min(4, 1, 4));
+
+console.log(Math.max(4, 1, 3, 10));
