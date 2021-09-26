9a8c91b5b240ef2864970919eed29854876cde39
Umang Sharma <umng01@gmail.com>
2021-09-26 12:58:56 +0530

    Js Parameters🇮🇩

---
+++ SheCodes Plus/Challenge19/src/index.js
@@ -0,0 +1,15 @@
+// Challenge
+// 1. Create a function isWindy
+// 2. Add a speed parameter to the function
+// 3. If speed is greater than 5, alert ‘It is windy’
+// 4. Else, alert ‘It is not windy’
+// 5. Call the function twice with different arguments to test both scenarios
+function isWindy(speed) {
+  if (speed > 5) {
+    alert("It is Windy");
+  } else {
+    alert("It is not Windy");
+  }
+}
+isWindy(3);
+isWindy(11);
